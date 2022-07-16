43d521b7f057fabca1048390fbd8890556b1608b
Kotorkovsciy <kotorkovsciy@gmail.com>
2022-07-17 00:16:05 +0300

    Corecting notifications

--- scripts/notifications.py
+++ scripts/notifications.py
@@ -8,7 +8,7 @@ async def scheduled(self):
         if await sql.newsJokesExists():
             for i in range(await sql.quantityUsers()):
                 row = await sql.newsJoke()
-                if row["user_id"] == i+1:
+                if row["user_id"] != i+1:
                     userId = await sql.infoId(i+1)
                     await bot.send_message(userId, f"Появилась новая шутка\n\n{row['joke']}\n\nАвтор: {row['author']}")
             await sql.deleteOldJoke()
