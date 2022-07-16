af6a359956589c2a683de8912acc7f7b3b78b0f6
Kotorkovsciy <kotorkovsciy@gmail.com>
2022-07-17 00:13:33 +0300

    Correct admin

--- handlers/admin.py
+++ handlers/admin.py
@@ -4,9 +4,10 @@ from os import getenv
 
 
 async def clearc(message: types.Message):
-    if int(getenv("ID_ADMIN")) == message.from_user.id:
-        await sql.deleteJokes()
-        await message.answer('Была произведена очистка')
+    if message.chat.type == 'private':
+        if int(getenv("ID_ADMIN")) == message.from_user.id:
+            await sql.deleteJokes()
+            await message.answer('Была произведена очистка')
 
 
 def register_handlers_admin(dp: Dispatcher):
