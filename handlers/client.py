1a830b27a6d679b9e06623f8415694e58efb777e
Kotorkovsciy <kotorkovsciy@gmail.com>
2022-07-17 16:43:34 +0300

    update client

--- handlers/client.py
+++ handlers/client.py
@@ -3,13 +3,18 @@ from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters import Text
 from aiogram.dispatcher.filters.state import State, StatesGroup
 from create_bot import sql, Anekdot
-from keyboards import kb_client, kb_record
+from keyboards import kb_client, kb_record, kb_aon
 
 
 class record(StatesGroup):
     quantity = State()
     joke = State()
     author = State()
+
+
+class delete(StatesGroup):
+    user_id = State()
+    aon = State()
 
 
 async def cmd_start(message: types.Message):
@@ -32,17 +37,35 @@ async def my_joke(message: types.Message):
     await message.answer(await sql.myJoke(message.from_user.id))
 
 
-async def delet_joke(message: types.Message):
-    await sql.deleteJokesUser(message.from_user.id)
-    await message.answer("Шутки удалены")
+async def delet_step(message: types.Message, state: FSMContext):
+    await delete.user_id.set()
+    await state.update_data(user_id=message.from_user.id)
+    await message.answer("Вы уверены ?", reply_markup=kb_aon)
+    await delete.aon.set()
+
+
+async def delete_res(message: types.Message, state: FSMContext):
+    await state.update_data(aon=message.text.lower())
+    user_data = await state.get_data()
+    match user_data["aon"]:
+        case "да":
+            await sql.deleteJokesUser(user_data["user_id"])
+            await state.finish()
+            await message.answer("🗑 Шутки удалены 🗑", reply_markup=kb_client)
+        case "нет":
+            await state.finish()
+            await message.answer("Действие отменено", reply_markup=kb_client)
+        case _:
+            await message.answer("Да или Нет", reply_markup=kb_aon)
+            return
 
 
 async def joke_step(message: types.Message, state: FSMContext):
-    await message.answer(text='Напиши шутку', reply_markup=kb_record)
     await record.quantity.set()
     quantity = await sql.quantityJokesUser(message.from_user.id)
     await state.update_data(quantity=quantity)
     if quantity < 10:
+        await message.answer(text='Напиши шутку', reply_markup=kb_record)
         await record.joke.set()
     else:
         await state.finish()
@@ -70,12 +93,17 @@ async def res_step(message: types.Message, state: FSMContext):
 
 def register_handlers_client(dp: Dispatcher):
     dp.register_message_handler(cmd_start, commands="start")
+    dp.register_message_handler(cmd_start, Text(
+        equals="В главное меню"))
     dp.register_message_handler(random_bot_joke, Text(
         equals="Шутку пользователей бота"))
     dp.register_message_handler(random_joke, Text(
         equals="Шутку рандомную из инета"))
     dp.register_message_handler(my_joke, Text(equals="Мои шутки"))
-    dp.register_message_handler(delet_joke, Text(equals="Удалить мои Шутки"))
+    dp.register_message_handler(delet_step, Text(
+        equals="Удалить мои Шутки"), state="*")
+    dp.register_message_handler(
+        delete_res, state=delete.aon, content_types=types.ContentTypes.TEXT)
     dp.register_message_handler(joke_step, Text(
         equals="Записать шутку"), state="*")
     dp.register_message_handler(cmd_cancel, commands="cancel", state="*")
